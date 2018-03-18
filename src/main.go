a7d44bde0877865f29fbe6d495a8d9e3fb0fb7f3
Gauthier Brion <gauthier.brion@gmail.com>
2018-03-18 11:13:16 +0100

    Update the way to wait interrupt signal

--- src/main.go
+++ src/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"encoding/hex"
-	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,43 +9,6 @@ import (
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
-
-func processFrames(fc <-chan xbee.Frame) {
-	var err error
-	for f := range fc {
-		switch f.Type {
-		case xbee.TypeReceivePacket:
-			err = processReceivePacketFrame(f)
-		default:
-			log.Printf("Unsupported frame type: %X\n", f.Type)
-		}
-		if err != nil {
-			log.Println(err)
-		}
-		log.Println("==============================================================")
-	}
-}
-
-func processReceivePacketFrame(f xbee.Frame) error {
-	sa64 := f.Data[:8]
-	log.Printf("64-bit source address: % X", sa64)
-
-	sa16 := f.Data[8:10]
-	log.Printf("16-bit source address: % X", sa16)
-
-	ro := f.Data[10:11]
-	log.Printf("Receive options: % X", ro)
-
-	rfd := f.Data[11:]
-	log.Printf("RF data: % X", rfd)
-	log.Printf("RF data (string): %s", rfd)
-
-	if err := redirect(hex.EncodeToString(sa64), rfd); err != nil {
-		return err
-	}
-
-	return nil
-}
 
 func init() {
 	viper.SetConfigName("config")
@@ -77,12 +38,11 @@ func main() {
 	var gracefulStop = make(chan os.Signal)
 	signal.Notify(gracefulStop, syscall.SIGTERM)
 	signal.Notify(gracefulStop, syscall.SIGINT)
-	go func() {
-		sig := <-gracefulStop
-		fmt.Printf("caught sig: %+v", sig)
-		close(frameChan)
-	}()
 
 	go xbee.ReadSerial(frameChan, viper.GetString("serial.name"), viper.GetInt("serial.speed"))
-	processFrames(frameChan)
+	go processFrames(frameChan)
+
+	sig := <-gracefulStop
+	log.Infof("caught sig: %+v", sig)
+	close(frameChan)
 }
---
+++ src/zb_process.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/hex"
+	"log"
+
+	"github.com/redkite1/zigbee-gw/src/xbee"
+)
+
+func processFrames(fc <-chan xbee.Frame) {
+	var err error
+	for f := range fc {
+		switch f.Type {
+		case xbee.TypeReceivePacket:
+			err = processReceivePacketFrame(f)
+		default:
+			log.Printf("Unsupported frame type: %X\n", f.Type)
+		}
+		if err != nil {
+			log.Println(err)
+		}
+		log.Println("==============================================================")
+	}
+}
+
+func processReceivePacketFrame(f xbee.Frame) error {
+	sa64 := f.Data[:8]
+	log.Printf("64-bit source address: % X", sa64)
+
+	sa16 := f.Data[8:10]
+	log.Printf("16-bit source address: % X", sa16)
+
+	ro := f.Data[10:11]
+	log.Printf("Receive options: % X", ro)
+
+	rfd := f.Data[11:]
+	log.Printf("RF data: % X", rfd)
+	log.Printf("RF data (string): %s", rfd)
+
+	if err := redirect(hex.EncodeToString(sa64), rfd); err != nil {
+		return err
+	}
+
+	return nil
+}
