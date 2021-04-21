2f404d7effe7e0dea42452ea0449b1aaf2134b1a
Gauthier Brion <gauthier@brion.be>
2021-04-21 21:56:00 +0200

    comment MQTT part

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"os/signal"
 	"syscall"
 
-	"github.com/redkite1/zigbee-gw/mqtt"
 	"github.com/redkite1/zigbee-gw/xbee"
 
 	log "github.com/sirupsen/logrus"
@@ -42,7 +41,7 @@ func main() {
 	ZBstopped := make(chan bool)
 
 	xbee.InitSerial(viper.GetString("serial.name"), viper.GetInt("serial.speed"))
-	mqtt.InitMQTT(viper.GetString("mqtt.host"), viper.GetInt("mqtt.port"), viper.GetString("mqtt.username"), viper.GetString("mqtt.password"))
+	//mqtt.InitMQTT(viper.GetString("mqtt.host"), viper.GetInt("mqtt.port"), viper.GetString("mqtt.username"), viper.GetString("mqtt.password"))
 
 	go xbee.ReadSerial(ZBframeChan)
 	go processZBFrames(ZBframeChan, ZBstopped)
@@ -63,7 +62,7 @@ func main() {
 	log.Info("Stopping gracefully the application...")
 
 	// Disconnect from MQTT
-	mqtt.Client.Disconnect(5000)
+	// mqtt.Client.Disconnect(5000)
 	// Stop processing more ZigBee frames
 	close(ZBframeChan)
 	// Stop processing more TCP requests
